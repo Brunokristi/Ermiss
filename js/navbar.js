5c4db6cbcdbaad168f02e41c8fb874e6f17741c8
Bruno <brunokristian003@gmail.com>
2024-09-14 13:31:43 +0200

    navbar done, mobile navbar done

---
+++ js/navbar.js
@@ -0,0 +1,8 @@
+function myFunction() {
+  var mobile = document.getElementById("mobile_menu");
+  if (mobile.style.display === "block") {
+    mobile.style.display = "none";
+  } else {
+    mobile.style.display = "block";
+  }
+}
